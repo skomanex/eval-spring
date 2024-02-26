1ad003624249b5fd51c679d1a1c6b9096b64b2ab
j.volland <volland.jimmy@gmail.com>
2024-02-26 11:16:40 +0100

    ajout dossier MDB pour mise en forme des htmls (dans static pour l'instant)

--- src/main/kotlin/com/example/evalspring/controllers/WebController.kt
+++ src/main/kotlin/com/example/evalspring/controllers/WebController.kt
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.GetMapping
 
 @Controller
 class WebController {
-
     //http://localhost:8080/home
     //http://141.94.237.180:8080/home
     @GetMapping("/home")
