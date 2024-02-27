a075c5f5eeeb307260124c9c2adde822c996a20b
j.volland <volland.jimmy@gmail.com>
2024-02-27 16:11:59 +0100

    changé le path vers la page home de http://141.94.237.180:8080/home à http://141.94.237.180:8080/

--- src/main/kotlin/com/example/evalspring/controllers/WebController.kt
+++ src/main/kotlin/com/example/evalspring/controllers/WebController.kt
@@ -5,9 +5,9 @@ import org.springframework.web.bind.annotation.GetMapping
 
 @Controller
 class WebController {
-    //http://localhost:8080/home
-    //http://141.94.237.180:8080/home
-    @GetMapping("/home")
+    //http://localhost:8080/
+    //http://141.94.237.180:8080/
+    @GetMapping("")
     fun home(): String{
         return "home"
     }
